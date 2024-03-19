669fa1e033886b7a7a8b4d6873bd62784440965f
Aryan Patel <21cs038@charusat.edu.in>
2024-03-19 11:52:09 +0530

    this should work

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -113,17 +113,18 @@ const Form = () => {
           <select
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-2"
             onChange={(e) => {
-              setUserData({ ...userdata, language: e.target.value });
+              const selectedLanguageId = e.target.value;
+              setLanguageId(selectedLanguageId);
+              const selectedLanguage = languages.find(
+                (language) => language.id === selectedLanguageId
+              );
+              if (selectedLanguage) {
+                setUserData({ ...userdata, language: selectedLanguage.vs });
+              }
             }}
           >
             {languages.map((language) => (
-              <option
-                value={language.vs}
-                key={language.id}
-                onClick={() => {
-                  setLanguageId(language.id);
-                }}
-              >
+              <option value={language.id} key={language.id}>
                 {language.name}
               </option>
             ))}
