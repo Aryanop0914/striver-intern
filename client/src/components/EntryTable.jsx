9f6350d8d7bb34fdd21436fa2138fdc3b88c12e4
Aryan Patel <21cs038@charusat.edu.in>
2024-03-19 13:45:17 +0530

    array reverse

--- client/src/components/EntryTable.jsx
+++ client/src/components/EntryTable.jsx
@@ -64,7 +64,7 @@ const EntryTable = () => {
             </tr>
           </thead>
           <tbody>
-            {userdetails.map((user) => (
+            {userdetails.reverse().map((user) => (
               <tr
                 className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
                 key={user.id}
